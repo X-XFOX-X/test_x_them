f3aa297010b9ed6de15f3419bbaa7d96fdb89c3d
Your Name <you@example.com>
2025-11-25 18:25:18 +0330

    .

--- functions.php
+++ functions.php
@@ -1,6 +1,8 @@
 <?php  // functions.php
 add_action('after_setup_theme', function () {
     add_theme_support('post-thumbnails');
+    add_theme_support('title-tag');
+
 });
 
 // // add style in enqueue
--- header.php
+++ header.php
@@ -3,15 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>
-<?php
-if ( is_singular() ) {
-    echo get_the_title() . ' | ' . get_bloginfo('name');
-} else {
-    bloginfo('name');
-}
-?>
-</title>
+
 
     <link rel="icon" href="<?= get_template_directory_uri()?>/assets/images/favicon.png">
     <link rel="apple-touch-icon" href="<?= get_template_directory_uri()?>/assets/images/favicon.png">
